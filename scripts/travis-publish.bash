3b10c4fdab3ba716cb5f3343a5ccf7d2aa249ddd
Sebastian Wiesner <sebastian.wiesner@gutefrage.net>
2017-01-04 11:22:25 +0100

    Only publish snapshot versions on Travis

--- scripts/travis-publish.bash
+++ scripts/travis-publish.bash
@@ -8,7 +8,7 @@ set -e
 # - we're building from the master branch, and
 # - have credentials available in secure env variables.
 function publish {
-  if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" && $TRAVIS_SECURE_ENV_VARS == "true" ]]; then
+  if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" && $TRAVIS_SECURE_ENV_VARS == "true" && $(cat version.sbt) =~ "-SNAPSHOT" ]]; then
     sbt publish
   fi
 }
